fb1efaca194782c1729548cca4c83409139a78b6
Daniel Yang <danielwzyang@gmail.com>
2024-11-20 16:29:36 -0500

    added dialog for description and time of event

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -3,17 +3,25 @@
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import { useEffect, useState } from "react"
+import { Dialog, DialogContent, DialogTitle, DialogDescription } from "./dialog"
+import dayjs from "dayjs"
+import customParseFormat from "dayjs/plugin/customParseFormat"
+
+dayjs.extend(customParseFormat)
 
 interface Event {
     start: {
         date: string,
         dateTime: string
     },
-    summary: string
+    summary: string,
+    description: string
 }
 
 export default function Events() {
     const [events, setEvents] = useState([])
+    const [open, setOpen] = useState(false)
+    const [curEvent, setEvent] = useState({ title: "", desc: "", time: "" })
 
     useEffect(() => {
         async function fetchEvents() {
@@ -26,10 +34,12 @@ export default function Events() {
 
             const data = await response.json()
 
-            const eventList = data.events.map((( e: Event ) => {
+            const eventList = data.events.map(((e: Event) => {
                 const date = e.start.date || e.start.dateTime.substring(0, 10)
+                const eventTime = e.start.dateTime ? e.start.dateTime.substring(11, 16) : ""
+                const desc = e.description || "no description"
 
-                return { title: e.summary, date }
+                return { title: e.summary, date, desc, eventTime }
             }))
 
             setEvents(eventList)
@@ -41,13 +51,26 @@ export default function Events() {
     return <div className="w-[80vw] max-w-[1000px]">
         <FullCalendar
             plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
             events={events}
             fixedWeekCount={false}
             contentHeight="auto"
             headerToolbar={{
                 right: "prev,next"
             }}
+            eventClick={(info) => {
+                const event = info.event
+                const props = event.extendedProps
+                const time = props.eventTime ? dayjs(props.eventTime, "HH:mm").format("h:mm A") : ""
+                setOpen(true)
+                setEvent({ title: event.title, desc: props.desc, time })
+            }}
         />
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="w-[300px] rounded-xl gap-0 bg-[#E9DCC9]">
+                <DialogTitle>{curEvent.title}</DialogTitle>
+                <h1 className="mb-2">{curEvent.time}</h1>
+                <DialogDescription>{curEvent.desc}</DialogDescription>
+            </DialogContent>
+        </Dialog>
     </div>
 }
