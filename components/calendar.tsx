0d87edd4b0c320b7b298eddab5e0b5664ce21496
Daniel Yang <danielwzyang@gmail.com>
2024-11-20 21:46:46 -0500

    added ending times and event descriptions can have line breaks

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -15,6 +15,9 @@ interface Event {
         date: string,
         dateTime: string
     },
+    end: {
+        dateTime: string
+    }
     summary: string,
     description: string
 }
@@ -22,7 +25,7 @@ interface Event {
 export default function Events() {
     const [events, setEvents] = useState([])
     const [open, setOpen] = useState(false)
-    const [curEvent, setEvent] = useState({ title: "", desc: "", time: "" })
+    const [curEvent, setEvent] = useState({ title: "", desc: "", beginTime: "", finishTime: "" })
     const [view] = useState(
         typeof window !== "undefined" && window.innerWidth < 700 ? "listMonth" : "dayGridMonth"
     )
@@ -40,10 +43,11 @@ export default function Events() {
 
             const eventList = data.events.map(((e: Event) => {
                 const date = e.start.date || e.start.dateTime.substring(0, 10)
-                const eventTime = e.start.dateTime ? e.start.dateTime.substring(11, 16) : ""
+                const beginTime = e.start.dateTime ? e.start.dateTime.substring(11, 16) : ""
+                const finishTime = e.end.dateTime ? e.end.dateTime.substring(11, 16) : ""
                 const desc = e.description || "no description"
 
-                return { title: e.summary, date, desc, eventTime }
+                return { title: e.summary, date, desc, beginTime, finishTime }
             }))
 
             setEvents(eventList)
@@ -64,9 +68,10 @@ export default function Events() {
             eventClick={(info) => {
                 const event = info.event
                 const props = event.extendedProps
-                const time = props.eventTime ? dayjs(props.eventTime, "HH:mm").format("h:mm A") : ""
+                const beginTime = props.beginTime ? dayjs(props.beginTime, "HH:mm").format("h:mm A") : ""
+                const finishTime = props.finishTime ? dayjs(props.finishTime, "HH:mm").format("h:mm A") : ""
                 setOpen(true)
-                setEvent({ title: event.title, desc: props.desc, time })
+                setEvent({ title: event.title, desc: props.desc, beginTime, finishTime })
             }}
             initialView={view}
             displayEventTime={false}
@@ -80,8 +85,8 @@ export default function Events() {
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent className="w-[300px] rounded-xl gap-0 bg-[#E9DCC9]">
                 <DialogTitle>{curEvent.title}</DialogTitle>
-                <h1 className="mb-2">{curEvent.time}</h1>
-                <DialogDescription>{curEvent.desc}</DialogDescription>
+                {curEvent.beginTime ? <h1 className="mb-2">{curEvent.beginTime} - {curEvent.finishTime}</h1> : <></>}
+                <p className="whitespace-pre-line">{curEvent.desc}</p>
             </DialogContent>
         </Dialog>
     </div>
