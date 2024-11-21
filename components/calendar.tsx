80c1e5fc5255534ae79f7120cc068c2f89d0d0ff
Daniel Yang <danielwzyang@gmail.com>
2024-11-20 21:48:00 -0500

    removed dialogdescription unused var

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -4,7 +4,7 @@ import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import listPlugin from "@fullcalendar/list"
 import { useEffect, useState } from "react"
-import { Dialog, DialogContent, DialogTitle, DialogDescription } from "./dialog"
+import { Dialog, DialogContent, DialogTitle } from "./dialog"
 import dayjs from "dayjs"
 import customParseFormat from "dayjs/plugin/customParseFormat"
 
