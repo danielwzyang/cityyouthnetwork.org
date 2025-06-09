4f41735f9a18157b398cec3c31d7b8acace24550
Daniel Yang <danielwzyang@gmail.com>
2025-06-09 16:52:32 -0400

    more lint errors

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
-import Link from "next/link"
-import Layout from "@/components/layout"
+import Layout from "../../components/layout"
 
 export default function Contact() {
     return <Layout>
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import Layout from "@/components/layout"
+import Layout from "../../components/layout"
 
 export default function Gallery() {
     return <Layout>
