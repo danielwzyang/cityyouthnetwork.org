9f0bd5a4d0d4b3d24770fee6138afb4a89cb1452
Daniel Yang <danielwzyang@gmail.com>
2025-06-16 15:04:37 -0400

    gallery photos

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,51 +1,53 @@
 import Image from "next/image"
 import Layout from "../../components/layout"
 
+const images = [
+    "/gallery/iceskating/1.jpg",
+    "/gallery/iceskating/2.jpg",
+    "/gallery/iceskating/3.jpg",
+    "/gallery/iceskating/4.jpg",
+    "/gallery/iceskating/5.jpg",
+
+    "/gallery/sda/1.jpg",
+    "/gallery/sda/2.jpg",
+    "/gallery/sda/3.jpg",
+    "/gallery/sda/4.jpg",
+    "/gallery/sda/5.jpg",
+    "/gallery/sda/6.jpg",
+    "/gallery/sda/7.jpg",
+
+    "/gallery/sixflags/1.jpg",
+    "/gallery/sixflags/2.jpg",
+    "/gallery/sixflags/3.jpg",
+    "/gallery/sixflags/4.jpg",
+
+    "/gallery/wollman/1.jpg",
+    "/gallery/wollman/2.jpg",
+
+    "/gallery/stjude/1.jpg",
+    "/gallery/stjude/2.jpg",
+
+    "/gallery/roosevelt/1.jpg",
+    "/gallery/roosevelt/2.jpg",
+    "/gallery/roosevelt/3.jpg",
+]
+
 export default function Gallery() {
     return <Layout>
-        <div className="pb-5">
-            <h1 className="text-center text-3xl sm:text-4xl font-extrabold grotesque mb-2">
-                Gallery
-            </h1>
-        </div>
-
-        <div className="relative w-full h-[80vh] overflow-hidden">
-            <Image
-                src="/cover.png"
-                alt="cover"
-                fill
-                className="object-cover"
-                priority
-            />
-
-            <svg
-                className="absolute top-0 left-0 w-full h-16"
-                viewBox="0 0 1440 80"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-            >
-                <path
-                    d="M0,50 C320,0 640,100 960,50 C1280,0 1600,100 1920,50 L1920,0 L0,0 Z"
-                    fill="#f2f1ea"
-                />
-            </svg>
-
-            <svg
-                className="absolute bottom-0 left-0 w-full h-24"
-                viewBox="0 0 1920 120"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-            >
-                <path
-                    d="M0,70 C320,120 640,20 960,70 C1280,120 1600,20 1920,70 L1920,120 L0,120 Z"
-                    fill="#f2f1ea"
-                />
-            </svg>
-        </div>
-
-        <div className="p-8 flex flex-col items-center">
+        <div className="p-8 w-full">
+            <div className="columns-1 sm:columns-2 lg:columns-3 gap-5">
+                {images.map((src, i) => (
+                    <div key={i} className="w-full relative rounded">
+                        <Image
+                            src={src}
+                            alt={src}
+                            width={600}
+                            height={400} 
+                            className="rounded block w-full mb-5"
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     </Layout>
 }
